e13607e7ba1397af73e3fbeb31c35db3815b4a37
Márton Torner <torner.marton@gmail.com>
2023-03-02 17:59:48 +0100

    feat: update git configs

--- bash/configure/git.sh
+++ bash/configure/git.sh
@@ -1,8 +1,7 @@
 git config --global user.name "Márton Torner"
 git config --global user.email torner.marton@gmail.com
 git config --global pull.ff only
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.st status
-git config --global alias.hist "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short"
 git config --global core.excludesfile ~/.gitignore
+git config --global advice.addEmptyPathspec false
+
+git config --global alias.unstage "reset"
