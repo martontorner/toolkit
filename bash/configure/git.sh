e4a91a9a43a915464be89ed9ba314cbf6cc0251c
Márton Torner <torner.marton@gmail.com>
2024-01-18 15:28:51 +0100

    feat(bash): update git configs and alisases

--- bash/configure/git.sh
+++ bash/configure/git.sh
@@ -2,11 +2,15 @@
 
 git config --global user.name "Márton Torner"
 git config --global user.email torner.marton@gmail.com
+
+git config --global core.editor vim
+
+git config --global init.defaultbranch master
+
 git config --global pull.ff only
-git config --global core.excludesfile ~/.gitignore
+
+git config --global push.autosetupremote true
+
 git config --global advice.addEmptyPathspec false
 
 git config --global alias.unstage "reset"
-
-git config --global init.defaultbranch master
-git config --global core.editor vim
