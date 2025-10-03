84440e3ea4faf309b4426f112603a518e7d37c71
Márton Torner <torner.marton@gmail.com>
2025-10-04 00:42:30 +0200

    refactor: further generalize prompts before merge

--- bash/tools/prompt.sh
+++ bash/tools/prompt.sh
@@ -8,46 +8,34 @@ __prompt_length () {
   echo -e "$1" | sed -r 's/\\\[[^]]*\\\]//g' | { read -r s; echo "${#s}"; }
 }
 
-_with_color () {
+__with_color () {
   echo "\[\e[${1}m\]"
 }
 
-_with_print () {
+__with_print () {
   echo "${1}"
 }
 
-_with_merge() {
-  local line_s=$1
-  local line_e=$2
-
-  line_s_length=$(__prompt_length "${line_s}")
-  line_e_length=$(__prompt_length "${line_e}")
-  line_c_length=$((COLUMNS - line_s_length - line_e_length))
-
-  line_c="$(printf '%*s' "$line_c_length" '')"
-
-  echo "${line_s}${line_c}${line_e}"
-}
-
-_set_options () {
+__set_options () {
   :
 }
 
-_unset_options () {
+__unset_options () {
   :
 }
 
 # --- HELPERS --- #
+# --- COMMONS --- #
 
 _host_status () {
   local line=""
 
-  line="${line}$(_with_color "0;38;5;31;48;49;22")"
-  line="${line}$(_with_print "")"
-  line="${line}$(_with_color "0;38;5;231;48;5;31")"
-  line="${line}$(_with_print " $(hostname) ")"
-  line="${line}$(_with_color "0;38;5;31;48;5;166;22")"
-  line="${line}$(_with_print "")"
+  line="${line}$(__with_color "0;38;5;31;48;49;22")"
+  line="${line}$(__with_print "")"
+  line="${line}$(__with_color "0;38;5;231;48;5;31")"
+  line="${line}$(__with_print " $(hostname) ")"
+  line="${line}$(__with_color "0;38;5;31;48;5;166;22")"
+  line="${line}$(__with_print "")"
 
   echo "${line}"
 }
@@ -55,10 +43,10 @@ _host_status () {
 _user_status () {
   local line=""
 
-  line="${line}$(_with_color "0;38;5;229;48;5;166")"
-  line="${line}$(_with_print "  $(whoami) ")"
-  line="${line}$(_with_color "0;38;5;166;48;5;240;22")"
-  line="${line}$(_with_print "")"
+  line="${line}$(__with_color "0;38;5;229;48;5;166")"
+  line="${line}$(__with_print "  $(whoami) ")"
+  line="${line}$(__with_color "0;38;5;166;48;5;240;22")"
+  line="${line}$(__with_print "")"
 
   echo "${line}"
 }
@@ -78,13 +66,13 @@ _path_status () {
     PARTS[0]="/"
   fi
 
-  line="${line}$(_with_color "0;38;5;250;48;5;240")"
+  line="${line}$(__with_color "0;38;5;250;48;5;240")"
 
   if [ ${#PARTS[@]} -gt 4 ]; then
-    line="${line}$(_with_print " ... ")"
-    line="${line}$(_with_color "0;38;5;245;48;5;240;22")"
-    line="${line}$(_with_print "")"
-    line="${line}$(_with_color "0;38;5;250;48;5;240")"
+    line="${line}$(__with_print " ... ")"
+    line="${line}$(__with_color "0;38;5;245;48;5;240;22")"
+    line="${line}$(__with_print "")"
+    line="${line}$(__with_color "0;38;5;250;48;5;240")"
 
     PARTS=("${PARTS[@]: -3}")
   fi
@@ -92,14 +80,14 @@ _path_status () {
   length=${#PARTS[@]}
   if [ "${length}" -gt 0 ]; then
     for part in "${PARTS[@]::${length}-1}"; do
-      line="${line}$(_with_print " ${part} ")"
-      line="${line}$(_with_color "0;38;5;245;48;5;240;22")"
-      line="${line}$(_with_print "")"
-      line="${line}$(_with_color "0;38;5;250;48;5;240")"
+      line="${line}$(__with_print " ${part} ")"
+      line="${line}$(__with_color "0;38;5;245;48;5;240;22")"
+      line="${line}$(__with_print "")"
+      line="${line}$(__with_color "0;38;5;250;48;5;240")"
     done
 
-    line="${line}$(_with_color "0;38;5;252;48;5;240;1")"
-    line="${line}$(_with_print " ${PARTS[${length}-1]} ")"
+    line="${line}$(__with_color "0;38;5;252;48;5;240;1")"
+    line="${line}$(__with_print " ${PARTS[${length}-1]} ")"
   fi
 
   echo "${line}"
@@ -111,8 +99,8 @@ _repo_status () {
   local branch=$(git branch 2> /dev/null | grep "\*" | sed -e "s/* \(.*\)/\1/")
 
   if [ "$branch" ]; then
-    line="${line}$(_with_color "0;38;5;240;48;5;236;22")"
-    line="${line}$(_with_print "")"
+    line="${line}$(__with_color "0;38;5;240;48;5;236;22")"
+    line="${line}$(__with_print "")"
 
     status_info="$(git status --branch --porcelain)"
 
@@ -120,56 +108,56 @@ _repo_status () {
     file_info="$(echo "${status_info}" | tail -n +2)"
 
     if [ "${file_info}" ]; then
-      line="${line}$(_with_color "0;38;5;247;48;5;236")"
+      line="${line}$(__with_color "0;38;5;247;48;5;236")"
     else
-      line="${line}$(_with_color "0;38;5;2;48;5;236")"
+      line="${line}$(__with_color "0;38;5;2;48;5;236")"
     fi
-    line="${line}$(_with_print "  ${branch} ")"
+    line="${line}$(__with_print "  ${branch} ")"
 
     behind="$(echo "${branch_info}" | sed -E "s/.*behind\ ([0-9]+).*/\1/" | sed -e "s/^##.*//")"
     if [ ! -z "${behind}" ]; then
-      line="${line}$(_with_color "0;38;5;252;48;5;236")"
-      line="${line}$(_with_print "↓ ${behind} ")"
+      line="${line}$(__with_color "0;38;5;252;48;5;236")"
+      line="${line}$(__with_print "↓ ${behind} ")"
     fi
 
     ahead="$(echo "${branch_info}" | sed -E "s/.*ahead\ ([0-9]+).*/\1/" | sed -e "s/^##.*//")"
     if [ ! -z "${ahead}" ]; then
-      line="${line}$(_with_color "0;38;5;252;48;5;236")"
-      line="${line}$(_with_print "↑ ${ahead} ")"
+      line="${line}$(__with_color "0;38;5;252;48;5;236")"
+      line="${line}$(__with_print "↑ ${ahead} ")"
     fi
 
     staged="$(echo "${file_info}" | grep -E "^([MRC].|[D][^D]|[A][^A])" | wc -l | sed -e "s/^[[:space:]]*//")"
     if [ "${staged}" -gt 0 ]; then
-      line="${line}$(_with_color "0;38;5;2;48;5;236")"
-      line="${line}$(_with_print "● ${staged} ")"
+      line="${line}$(__with_color "0;38;5;2;48;5;236")"
+      line="${line}$(__with_print "● ${staged} ")"
     fi
 
     unmerged="$(echo "${file_info}" | grep -E "^([U].|.[U]|[A][A]|[D][D])" | wc -l | sed -e "s/^[[:space:]]*//")"
     if [ "${unmerged}" -gt 0 ]; then
-      line="${line}$(_with_color "0;38;5;2;48;5;236")"
-      line="${line}$(_with_print "✖ ${unmerged} ")"
+      line="${line}$(__with_color "0;38;5;2;48;5;236")"
+      line="${line}$(__with_print "✖ ${unmerged} ")"
     fi
 
     modified="$(echo "${file_info}" | grep -E "^(.[M]|[^D][D])" | wc -l | sed -e "s/^[[:space:]]*//")"
     if [ "${modified}" -gt 0 ]; then
-      line="${line}$(_with_color "0;38;5;166;48;5;236")"
-      line="${line}$(_with_print "✚ ${modified} ")"
+      line="${line}$(__with_color "0;38;5;166;48;5;236")"
+      line="${line}$(__with_print "✚ ${modified} ")"
     fi
 
     untracked="$(echo "${file_info}" | grep "^??" | wc -l | sed -e "s/^[[:space:]]*//")"
     if [ "${untracked}" -gt 0 ]; then
-      line="${line}$(_with_color "0;38;5;214;48;5;236")"
-      line="${line}$(_with_print "… ${untracked} ")"
+      line="${line}$(__with_color "0;38;5;214;48;5;236")"
+      line="${line}$(__with_print "… ${untracked} ")"
     fi
 
     stashed="$(git stash list --no-decorate | wc -l | sed -e "s/^[[:space:]]*//")"
     if [ "${stashed}" -gt 0 ]; then
-      line="${line}$(_with_color "0;38;5;31;48;5;236")"
-      line="${line}$(_with_print "⚑ ${stashed} ")"
+      line="${line}$(__with_color "0;38;5;31;48;5;236")"
+      line="${line}$(__with_print "⚑ ${stashed} ")"
     fi
 
-    line="${line}$(_with_color "0;38;5;236;48;5;240;22")"
-    line="${line}$(_with_print "")"
+    line="${line}$(__with_color "0;38;5;236;48;5;240;22")"
+    line="${line}$(__with_print "")"
   fi
 
   echo "${line}"
@@ -181,15 +169,15 @@ _exit_status () {
   local exit_code=$1
 
   if [ "${exit_code}" -gt 0 ]; then
-    line="${line}$(_with_color "0;38;5;240;48;5;52;22")"
-    line="${line}$(_with_print "")"
-    line="${line}$(_with_color "0;38;5;231;48;5;52")"
-    line="${line}$(_with_print " ${exit_code} ")"
-    line="${line}$(_with_color "0;38;5;52;48;49;22")"
-    line="${line}$(_with_print "")"
+    line="${line}$(__with_color "0;38;5;240;48;5;52;22")"
+    line="${line}$(__with_print "")"
+    line="${line}$(__with_color "0;38;5;231;48;5;52")"
+    line="${line}$(__with_print " ${exit_code} ")"
+    line="${line}$(__with_color "0;38;5;52;48;49;22")"
+    line="${line}$(__with_print "")"
   else
-    line="${line}$(_with_color "0;38;5;240;48;49;22")"
-    line="${line}$(_with_print "")"
+    line="${line}$(__with_color "0;38;5;240;48;49;22")"
+    line="${line}$(__with_print "")"
   fi
 
   echo "${line}"
@@ -198,19 +186,19 @@ _exit_status () {
 _kube_status () {
   local line=""
 
-  line="${line}$(_with_color "0;38;5;240;48;49;22")"
-  line="${line}$(_with_print "")"
+  line="${line}$(__with_color "0;38;5;240;48;49;22")"
+  line="${line}$(__with_print "")"
 
   if command -v docker &> /dev/null; then
-    line="${line}$(_with_color "0;38;5;252;48;5;240;1")"
-    line="${line}$(_with_print "   ")"
+    line="${line}$(__with_color "0;38;5;252;48;5;240;1")"
+    line="${line}$(__with_print "   ")"
   fi
 
   local context=$(kubectl config current-context 2> /dev/null)
 
   if [ "${context}" ]; then
-    line="${line}$(_with_color "0;38;5;252;48;5;240;1")"
-    line="${line}$(_with_print "${context} ")"
+    line="${line}$(__with_color "0;38;5;252;48;5;240;1")"
+    line="${line}$(__with_print "${context} ")"
   fi
 
   echo "${line}"
@@ -231,53 +219,53 @@ _tool_status () {
   if [ "$python3_version" ]; then
     has_version=1
 
-    line="${line}$(_with_color "0;38;5;75;48;5;236")"
-    line="${line}$(_with_print " ")"
-    line="${line}$(_with_color "0;38;5;247;48;5;236")"
-    line="${line}$(_with_print " ${python3_version/Python /} ")"
+    line="${line}$(__with_color "0;38;5;75;48;5;236")"
+    line="${line}$(__with_print " ")"
+    line="${line}$(__with_color "0;38;5;247;48;5;236")"
+    line="${line}$(__with_print " ${python3_version/Python /} ")"
   elif [ "$python_version" ]; then
     has_version=1
 
-    line="${line}$(_with_color "0;38;5;75;48;5;236")"
-    line="${line}$(_with_print " ")"
-    line="${line}$(_with_color "0;38;5;247;48;5;236")"
-    line="${line}$(_with_print " ${python_version/Python /} ")"
+    line="${line}$(__with_color "0;38;5;75;48;5;236")"
+    line="${line}$(__with_print " ")"
+    line="${line}$(__with_color "0;38;5;247;48;5;236")"
+    line="${line}$(__with_print " ${python_version/Python /} ")"
   fi
 
   if [ "$node_version" ]; then
     if [ $has_version ]; then
-      line="${line}$(_with_color "0;38;5;247;48;5;236")"
-      line="${line}$(_with_print "")"
+      line="${line}$(__with_color "0;38;5;247;48;5;236")"
+      line="${line}$(__with_print "")"
     fi
 
     has_version=1
 
-    line="${line}$(_with_color "0;38;5;2;48;5;236")"
-    line="${line}$(_with_print " ")"
-    line="${line}$(_with_color "0;38;5;247;48;5;236")"
-    line="${line}$(_with_print " ${node_version/v/} ")"
+    line="${line}$(__with_color "0;38;5;2;48;5;236")"
+    line="${line}$(__with_print " ")"
+    line="${line}$(__with_color "0;38;5;247;48;5;236")"
+    line="${line}$(__with_print " ${node_version/v/} ")"
   fi
 
   if [ "$go_version" ]; then
     if [ $has_version ]; then
-      line="${line}$(_with_color "0;38;5;247;48;5;236")"
-      line="${line}$(_with_print "")"
+      line="${line}$(__with_color "0;38;5;247;48;5;236")"
+      line="${line}$(__with_print "")"
     fi
 
     has_version=1
 
-    line="${line}$(_with_color "0;38;5;39;48;5;236")"
-    line="${line}$(_with_print " 󰟓 ")"
-    line="${line}$(_with_color "0;38;5;247;48;5;236")"
-    line="${line}$(_with_print " ${go_version/go/} ")"
+    line="${line}$(__with_color "0;38;5;39;48;5;236")"
+    line="${line}$(__with_print " 󰟓 ")"
+    line="${line}$(__with_color "0;38;5;247;48;5;236")"
+    line="${line}$(__with_print " ${go_version/go/} ")"
   fi
 
   if [ $has_version ]; then
-    line="$(_with_print "")${line}"
-    line="$(_with_color "0;38;5;236;48;5;240;22")${line}"
+    line="$(__with_print "")${line}"
+    line="$(__with_color "0;38;5;236;48;5;240;22")${line}"
 
-    line="${line}$(_with_color "0;38;5;240;48;5;236;22")"
-    line="${line}$(_with_print "")"
+    line="${line}$(__with_color "0;38;5;240;48;5;236;22")"
+    line="${line}$(__with_print "")"
   fi
 
   echo "${line}"
@@ -286,50 +274,56 @@ _tool_status () {
 _time_status () {
   local line=""
 
-  line="${line}$(_with_color "0;38;5;252;48;5;240;1")"
-  line="${line}$(_with_print " $(date +'%I:%M %p')  ")"
+  line="${line}$(__with_color "0;38;5;252;48;5;240;1")"
+  line="${line}$(__with_print " $(date +'%I:%M %p')  ")"
 
-  line="${line}$(_with_color "0;38;5;240;48;49;22")"
-  line="${line}$(_with_print "")"
-  line="${line}$(_with_color "0")"
+  line="${line}$(__with_color "0;38;5;240;48;49;22")"
+  line="${line}$(__with_print "")"
+  line="${line}$(__with_color "0")"
 
   echo "${line}"
 }
 
 _create_status_line () {
-  local l_line=""
-  local r_line=""
+  local line_s=""
+  local line_e=""
 
-  l_line="${l_line}$(_host_status)"
-  l_line="${l_line}$(_user_status)"
-  l_line="${l_line}$(_path_status)"
-  l_line="${l_line}$(_repo_status)"
-  l_line="${l_line}$(_exit_status $1)"
+  line_s="${line_s}$(_host_status)"
+  line_s="${line_s}$(_user_status)"
+  line_s="${line_s}$(_path_status)"
+  line_s="${line_s}$(_repo_status)"
+  line_s="${line_s}$(_exit_status $1)"
 
-  r_line="${r_line}$(_kube_status)"
-  r_line="${r_line}$(_tool_status)"
-  r_line="${r_line}$(_time_status)"
+  line_e="${line_e}$(_kube_status)"
+  line_e="${line_e}$(_tool_status)"
+  line_e="${line_e}$(_time_status)"
 
-  echo "$(_with_merge "${l_line}" "${r_line}")"
+  line_s_length=$(__prompt_length "${line_s}")
+  line_e_length=$(__prompt_length "${line_e}")
+  line_c_length=$((COLUMNS - line_s_length - line_e_length))
+
+  echo "${line_s}$(printf '%*s' "$line_c_length" '')${line_e}"
 }
 
 _create_prompt_line () {
-  echo "$(_with_print " $ ")"
+  echo "$(__with_print " $ ")"
 }
 
 _update_prompt () {
   local line=""
 
-  _set_options
+  __set_options
 
   line="${line}$(_create_status_line $?)"
   line="${line}\n"
   line="${line}$(_create_prompt_line)"
 
-  _unset_options
+  __unset_options
 
   PS1=$(echo "${line}")
 }
+
+# --- COMMONS --- #
 
 if command -v pip &> /dev/null && command -v powerline-daemon &> /dev/null; then
   POWERLINE_ROOT=$(pip show powerline-status | grep Location | cut -d" " -f2)
@@ -345,7 +339,5 @@ if command -v pip &> /dev/null && command -v powerline-daemon &> /dev/null; then
   fi
 else
   # no powerline is detected -> use custom prompt
-  export VIRTUAL_ENV_DISABLE_PROMPT=1
-
   PROMPT_COMMAND=_update_prompt
 fi
--- shell/tools/variables.sh
+++ shell/tools/variables.sh
@@ -1,12 +1,15 @@
 export SHELL_TOOLKIT="shell"
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
 
 export IP_REGEX="(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
 export MAC_REGEX="[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}"
 
-export GPG_TTY=$(tty)
+export GPG_TTY="$(tty)"
 
 # Mac
 export BASH_SILENCE_DEPRECATION_WARNING=1
+
+# Python
+export VIRTUAL_ENV_DISABLE_PROMPT=1
