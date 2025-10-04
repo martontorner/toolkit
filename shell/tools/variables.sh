b53a9c2878001f78f4516a67dfcc0ed80053aeae
Márton Torner <torner.marton@gmail.com>
2025-10-04 19:13:33 +0200

    feat: add some variables

--- shell/tools/variables.sh
+++ shell/tools/variables.sh
@@ -3,6 +3,14 @@ export SHELL_TOOLKIT="shell"
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
+export HISTSIZE='32768';
+export HISTFILESIZE="${HISTSIZE}";
+export HISTCONTROL='ignoreboth';
+
+export EDITOR='vim';
+
+export MANPAGER='less -X';
+
 export IP_REGEX="(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
 export MAC_REGEX="[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}"
 
